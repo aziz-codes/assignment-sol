6d5d474b0670612d7658e40563e669f9a2923c76
aziz-codes <azizisoffline@gmail.com>
2023-03-13 23:27:27 +0500

    creating navlinks

---
+++ src/assets/dummy.js
@@ -0,0 +1,18 @@
+export const links = [
+  {
+    path: "/",
+    label: "Home",
+  },
+  {
+    path: "view-template",
+    label: "View Template",
+  },
+  {
+    path: "/generate-assignment",
+    label: "Generate Assignment",
+  },
+  {
+    path: "view-course",
+    label: "View Course",
+  },
+];
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,24 @@
-import React from "react";
-
+import { NavLink } from "react-router-dom";
+import { links } from "../assets/dummy";
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav className="bg-[#0b8d9f] w-full h-12">
+      <div className="w-full relative flex justify-start px-1 items-center h-full gap-6">
+        <div className="flex">
+          <h4 className="bg-black text-xl font-semibold text-[#0b8d9f]">
+            ASSIGNMENT
+          </h4>
+
+          <span className="bg-white font-semibold pr-0.5">SOL</span>
+        </div>
+        <div className="flex gap-5">
+          {links.map((link, index) => (
+            <NavLink key={index}>{link.label}</NavLink>
+          ))}
+        </div>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
