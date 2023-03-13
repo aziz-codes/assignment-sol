1193bfec8ee2e1d98a5edaf75b61721df8cc5388
aziz-codes <azizisoffline@gmail.com>
2023-03-14 00:59:59 +0500

    done with Navbar

--- src/components/MiniBar.jsx
+++ src/components/MiniBar.jsx
@@ -1,9 +1,12 @@
 import React from "react";
-
+import { HiOutlineMail } from "react-icons/hi";
 const MiniBar = () => {
   return (
     <div className="relative bg-black text-white w-full h-8 flex justify-center items-center">
-      <label>assignmentsol@gmail.com</label>
+      <div className="flex flex-row items-center h-full">
+        <HiOutlineMail />
+        <label className="text-xs">assignmentsol@gmail.com</label>
+      </div>
     </div>
   );
 };
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,25 @@
 import { NavLink } from "react-router-dom";
 import { links } from "../assets/dummy";
+import { AiFillCaretDown } from "react-icons/ai";
+import { useState } from "react";
+import { useLocation } from "react-router-dom";
 const Navbar = () => {
+  const location = useLocation();
+  const subMenu = [
+    {
+      path: "/about-us",
+      label: "About Us",
+    },
+    {
+      path: "contact-us",
+      label: "Contact Us",
+    },
+    {
+      path: "faqs",
+      label: "FAQ's",
+    },
+  ];
+  const [open, setOpen] = useState(false);
   return (
     <nav className="bg-[#0b8d9f] w-full h-12">
       <div className="w-full relative flex justify-start px-1 items-center h-full gap-6">
@@ -11,13 +30,52 @@ const Navbar = () => {
 
           <span className="bg-white font-semibold pr-0.5">SOL</span>
         </div>
-        <div className="flex gap-5">
-          {links.map((link, index) => (
-            <NavLink key={index} className="text-white">
-              {link.label}
+        {location.pathname === "/" ? (
+          <div className="flex gap-5">
+            {links.map((link, index) => (
+              <NavLink
+                key={index}
+                to={link.path}
+                className="text-white py-2 hover:bg-black px-0.5"
+              >
+                {link.label}
+              </NavLink>
+            ))}
+            <div
+              className="flex flex-row cursor-pointer text-white relative items-center hover:bg-black py-2"
+              onClick={() => {
+                setOpen(!open);
+              }}
+            >
+              <label className="cursor-pointer">Help</label>
+              <AiFillCaretDown className="text-white h-3 w-3 cursor-pointer" />
+              {open && (
+                <div className="absolute top-8 w-32  h-36 bg-white  text-black shadow-md">
+                  <div className="flex flex-col items-baseline h-full mt-1 justify-between">
+                    {subMenu.map((item, index) => (
+                      <NavLink
+                        key={index}
+                        to={item.path}
+                        className="w-full hover:bg-gray-200 px-3 py-3 items-center hover:text-amber-800 transition-all duration-100 ease-linear"
+                      >
+                        {item.label}
+                      </NavLink>
+                    ))}
+                  </div>
+                </div>
+              )}
+            </div>
+          </div>
+        ) : (
+          <div className="flex items-center gap-5">
+            <NavLink to="/" className="text-white py-2 hover:bg-black px-0.5">
+              Home
             </NavLink>
-          ))}
-        </div>
+            <label className="capitalize text-white">
+              {location.pathname.replace("/", "").replace("-", " ")}
+            </label>
+          </div>
+        )}
       </div>
     </nav>
   );
