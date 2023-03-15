33e7cd9eee8347b0c7a0728145a7907e61e16817
aziz-codes <azizisoffline@gmail.com>
2023-03-15 17:28:19 +0500

    Contact Us Page ready

--- src/pages/Contactus.jsx
+++ src/pages/Contactus.jsx
@@ -1,7 +1,53 @@
-import React from "react";
-
 const Contactus = () => {
-  return <div>Contactus</div>;
+  return (
+    <section className="flex lg:flex-row flex-col flex-wrap justify-center px-3 w-full my-6">
+      <div className="first-section flex flex-col gap-10 max-w-md relative top-20 pr-3">
+        <h1 className="text-[#0b8d9f] text-3xl italic">
+          Don't Be Shy, Talk to Us.
+        </h1>
+        <h4 className="text-md text-gray-400">
+          Here is a few approaches to get in touch with me, It would be ideal if
+          you send an email, fill the contact form.{" "}
+          <span className="font-bold text-black">
+            We are looking forward to speacking with you.
+          </span>
+        </h4>
+      </div>
+      <div className="second-section border w-[320px] max-w-sm shadow-md rounded-md h-auto py-2  flex flex-col gap-4 px-4 pb-6">
+        <h2 className="text-center font-semibold">Contact Me</h2>
+        <input
+          type="text"
+          placeholder="Name"
+          className="border-b focus:outline-none outline-none py-1"
+        />
+        <input
+          type="email"
+          placeholder="Email"
+          className="border-b focus:outline-none outline-none py-1"
+        />
+        <input
+          type="phone"
+          placeholder="Phone"
+          className="border-b focus:outline-none outline-none py-1"
+        />
+        <select className="border-b outline-none text-gray-400">
+          <option disabled selected>
+            What are you getting in touch with?
+          </option>
+          <option>Account</option>
+          <option>Courses</option>
+          <option>Assignemnt</option>
+        </select>
+        <textarea
+          placeholder="Message"
+          className="border-b outline-none"
+        ></textarea>
+        <button className="px-1 border-none bg-black text-white font-bold p-1 rounded-md py-2 w-32 mx-auto">
+          Send Message
+        </button>
+      </div>
+    </section>
+  );
 };
 
 export default Contactus;
