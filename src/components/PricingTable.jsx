e7d8a9d74e786929efabf7cc6492c7c9e3766d13
aziz-codes <azizisoffline@gmail.com>
2023-03-19 00:40:01 +0500

    group hover

--- src/components/PricingTable.jsx
+++ src/components/PricingTable.jsx
@@ -18,10 +18,10 @@ const PricingTable = () => {
   return (
     <section className="mt-20 flex flex-col gap-6 items-center">
       <h2 className="text-2xl font-bold ">Assignment Prices</h2>
-      <div className="w-full bg-[#f0f4fa] h-80 flex justify-center gap-10 items-center">
+      <div className="w-full bg-[#f0f4fa] h-80 flex justify-center gap-10 items-center ">
         {prices.map((item, i) => (
-          <div className="flex flex-col h-56 border w-52" key={i}>
-            <div className="w-full  bg-[#e1e4e1] flex flex-row justify-center items-center text-gray-500 cursor-pointer group p-3 hover:bg-[#0b8d9f]  hover:text-white">
+          <div className="flex flex-col h-56 border w-52 group" key={i}>
+            <div className=" w-full  bg-[#e1e4e1] flex flex-row justify-center items-center text-gray-500 cursor-pointer  p-3 hover:bg-[#0b8d9f]  hover:text-white">
               <h4 className="uppercase text-xl ">{item.type}</h4>
             </div>
             <div className="w-full flex-1 bg-[#f4f6f3] flex justify-center items-center">
@@ -29,7 +29,7 @@ const PricingTable = () => {
               <h4 className="text-5xl uppercase">{item.price}</h4>
             </div>
             <div className="w-full flex-1 bg-[#fcfffc] flex justify-center items-center">
-              <button className="border border-sky-500 rounded-2xl text-sky-500 px-2 text-sm py-1 w-auto uppercase group-hover:bg-[#0b8d9f]">
+              <button className="border border-sky-500 rounded-2xl text-sky-500 px-2 text-sm py-1 w-auto uppercase group-hover:bg-[#0b8d9f] group-hover:text-white">
                 generate assignment
               </button>
             </div>
